12a52389c2439c1fe1ad76d10f0dd1c79d2d645d
konekato <18ec033@ms.dendai.ac.jp>
2020-05-12 17:43:51 +0900

    fix: setup.py 曜日ごとに入力機能

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,10 @@ import config
 def setup():
     urlsdict = config.urlsdict
     for dow in config.DOW_LIST:
+        print(dow + "曜日")
         for i in range(5):
-            if (i+1) % 2 == 0:
-                urlsdict[dow][str(i+1)] = 'even'
-            else:
-                urlsdict[dow][str(i+1)] = 'odd'
+            print(str(i+1) + '限目: ')
+            urlsdict[dow][str(i+1)] = input()
 
     with open('src/data/urls.json', 'w') as f:
         json.dump(urlsdict, f, indent=2, ensure_ascii=False)
