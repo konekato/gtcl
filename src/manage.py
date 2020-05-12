fcb61d0231e1c56d2a0d137a04797fb49658578b
konekato <18ec033@ms.dendai.ac.jp>
2020-05-12 15:30:50 +0900

    add: openコマンドをpythonで

--- src/manage.py
+++ src/manage.py
@@ -1,1 +1,12 @@
-print("This is main")
+import subprocess
+
+def main():
+    url='https://google.com'
+    args = ['open', url]
+    try:
+        res = subprocess.check_output(args)
+    except:
+        print('Error.')
+
+if __name__ == '__main__':
+    main()
