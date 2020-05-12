7aeeb3bf3532440440acdc5c45133c5289090d4a
konekato <18ec033@ms.dendai.ac.jp>
2020-05-12 17:57:16 +0900

    add: json から抜き出してそれをopenする機能

--- src/manage.py
+++ src/manage.py
@@ -1,10 +1,19 @@
 import subprocess
+import json
 
 def main():
-    url='https://google.com'
+    print('いつが良い？')
+    print('曜日を選択してください。（ 火 のように入力してね。）')
+    dow = input()
+    print('時限を選択してください。（ 3 のように入力してね。）')
+    period = input()
+
+    urlsdict = json.load(open('src/data/urls.json', 'r'))
+
+    url = urlsdict[dow][period]
     args = ['open', url]
     try:
-        res = subprocess.check_output(args)
+        subprocess.check_output(args)
     except:
         print('Error.')
 
