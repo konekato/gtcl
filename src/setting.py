dfe1b153ab10fd2181ece015ddcb9184dc6519ea
konekato <18ec033@ms.dendai.ac.jp>
2020-05-12 22:53:55 +0900

    add: setting.py update() 追加

--- src/setting.py
+++ src/setting.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import json
 import config
 import input
@@ -17,6 +18,25 @@ def setup():
     except FileNotFoundError:
         print('-------------\nError.\nsrc/data ディレクトリが見つかりません。\nHint: $ mkdir src/data\n-------------')
         return
-    
 
-setup()
+def update():
+    print('設定内容を編集します。')
+    print('いつが良い？')
+    print('曜日を選択してください。（ 火 のように入力してね。）')
+    dow = input.dow_input()
+    print('時限を選択してください。（ 3 のように入力してね。）')
+    period = input.period_input()
+    print('更新する授業のZOOMのURLを入力してください。')
+    url = input.url_input()
+
+    try:
+        with open('src/data/urls.json') as f:
+            urlsdict = json.load(f, object_pairs_hook=OrderedDict)
+    except FileNotFoundError:
+        print('src/data/urls.json が見当たりません。\nHint: setup()\n')
+        return
+    
+    urlsdict[dow][period] = url
+
+    with open('src/data/urls.json', 'w') as f:
+        json.dump(urlsdict, f, indent=2, ensure_ascii=False)
