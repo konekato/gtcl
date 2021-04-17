ae403847cabc81748c3f45c4c0a91ea15db2352f
konekato <k1shock1125@gmail.com>
2021-04-17 19:28:41 +0900

    add: animation. And deploy js files

--- src/gtcl-for-browser/javascript/page-edit.js
+++ src/gtcl-for-browser/javascript/page-edit.js
@@ -1,4 +1,22 @@
 import {createTableHeader, createTableBodyForEdit} from './page.js';
 
-setTimeout(createTableHeader, 10);
-setTimeout(createTableBodyForEdit, 20);
+const s = 500;
+const bar = document.getElementById('loading-bar');
+for (let ms = 0; ms <= s; ms++) {
+    setTimeout(() => {
+        const p = ms * 100 / s;
+        bar.style.width = p + '%';
+    }, ms);
+}
+setTimeout(() => {
+    createTableHeader();
+    createTableBodyForEdit();
+
+    const hund2 = 500;
+    for (let ms = 0; ms <= hund2; ms++) {
+        setTimeout(() => {
+            const p = 100 - ms * 100 / hund2;
+            bar.style.opacity = p + '%';
+        }, ms);
+    }
+}, s+1);
--- src/gtcl-for-browser/javascript/page-home.js
+++ src/gtcl-for-browser/javascript/page-home.js
@@ -1,4 +1,22 @@
 import {createTableHeader, createTableBodyForHome} from './page.js';
 
-setTimeout(createTableHeader, 10);
-setTimeout(createTableBodyForHome, 20);
+const s = 500;
+const bar = document.getElementById('loading-bar');
+for (let ms = 0; ms <= s; ms++) {
+    setTimeout(() => {
+        const p = ms * 100 / s;
+        bar.style.width = p + '%';
+    }, ms);
+}
+setTimeout(() => {
+    createTableHeader();
+    createTableBodyForHome();
+
+    const hund2 = 200;
+    for (let ms = 0; ms <= hund2; ms++) {
+        setTimeout(() => {
+            const p = 100 - ms * 100 / hund2;
+            bar.style.opacity = p + '%';
+        }, ms);
+    }
+}, s+1);
